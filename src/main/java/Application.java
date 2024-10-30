81aeda0441acb4750f9d46976dfbb269cef26c75
Vaishnavi Gupta <muskan.vgupta306@gmail.com>
2024-10-30 19:46:25 +0530

    Add examples for bridge design pattern

---
+++ src/main/java/Application.java
@@ -0,0 +1,26 @@
+import abstraction.AdvancedRemote;
+import abstraction.BasicRemote;
+import implementation.Device;
+import implementation.Radio;
+import implementation.TV;
+
+public class Application {
+    public static void main(String[] args) {
+        testDevice(new TV());
+        testDevice(new Radio());
+    }
+
+    public static void testDevice(Device device) {
+        System.out.println("Tests with basic remote.");
+        BasicRemote basicRemote = new BasicRemote(device);
+        basicRemote.power();
+        device.printStatus();
+
+        System.out.println("Tests with advanced remote.");
+        AdvancedRemote advancedRemote = new AdvancedRemote(device);
+        advancedRemote.power();
+        advancedRemote.mute();
+        device.printStatus();
+
+    }
+}
---
+++ src/main/java/abstraction/Remote.java
@@ -0,0 +1,13 @@
+package abstraction;
+
+public interface Remote {
+    void power();
+
+    void volumeDown();
+
+    void volumeUp();
+
+    void channelDown();
+
+    void channelUp();
+}
